05ed300a2ef38ece50faa29b7157b2ddfa44e333
Tony Li <tonyli.lives@gmail.com>
2015-01-16 16:31:13 -0800

    added more tests

--- tests/DataDecoratorTest.php
+++ tests/DataDecoratorTest.php
@@ -34,7 +34,93 @@ class DataDecoratorTest extends AbstractTestCase
 
     public function testGet()
     {
-        $this->assertEquals($this->decorator->get('number'), 40);
-        $this->assertEquals($this->decorator->get('default', 'default'), 'default');
+        $this->assertEquals(40, $this->decorator->get('number'));
+        $this->assertEquals('this is a string', $this->decorator->get('string'));
+        $this->assertEquals(null, $this->decorator->get('null'));
+        $this->assertEquals('default', $this->decorator->get('default', 'default'));
+    }
+
+    public function testOnly()
+    {
+        $one = [
+            'number' => 40,
+        ];
+        $this->assertEquals($one, $this->decorator->only('number'));
+
+        $multi = [
+            'number' => 40,
+            'string' => 'this is a string',
+        ];
+        $this->assertEquals($multi, $this->decorator->only('number', 'string'));
+
+        $array = [
+            'number' => 40,
+            'string' => 'this is a string',
+        ];
+        $this->assertEquals($array, $this->decorator->only(['number', 'string']));
+
+        $missing = [
+            'number' => 40,
+            'bad' => null,
+        ];
+        $this->assertEquals($missing, $this->decorator->only('bad', 'number'));
+
+        $empty = [
+            'bad' => null,
+        ];
+        $this->assertEquals($empty, $this->decorator->only('bad'));
+    }
+
+    public function testExcept()
+    {
+        $one = [
+            'string' => 'this is a string',
+            'datetime' => new DateTime,
+        ];
+        $this->assertEquals($one, $this->decorator->except('number'));
+
+        $multi = [
+            'datetime' => new DateTime,
+        ];
+        $this->assertEquals($multi, $this->decorator->except('number', 'string'));
+
+        $array = [
+            'string' => 'this is a string',
+        ];
+        $this->assertEquals($array, $this->decorator->except('number', 'datetime'));
+
+        $missing = [
+            'number' => 40,
+            'string' => 'this is a string',
+        ];
+        $this->assertEquals($missing, $this->decorator->except('missing', 'datetime'));
+    }
+
+    public function testMerge()
+    {
+        $add = [
+            'array' => [
+                'a', 'b',
+            ],
+            'string' => 'string has changed',
+            'no-key',
+        ];
+        $this->decorator->merge($add);
+
+        $sum = [
+            'number' => 40,
+            'datetime' => new DateTime,
+            'array' => [
+                'a', 'b',
+            ],
+            'string' => 'string has changed',
+            'no-key',
+        ];
+        $this->assertEquals($sum, $this->decorator->all());
+
+        $nested = [
+            'a', 'b',
+        ];
+        $this->assertEquals($nested, $this->decorator->get('array'));
     }
 }
